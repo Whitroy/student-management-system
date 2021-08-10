4c135bc020f2ffbf682440ce333642867994290b
Whitroy <55481516+Whitroy@users.noreply.github.com>
2021-08-10 20:37:33 +0530

    removing warnings

--- src/components/SideBar/ExpandableNavLink.tsx
+++ src/components/SideBar/ExpandableNavLink.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { HiChevronUp } from "react-icons/hi";
 import H2 from "../Basic/H2";
